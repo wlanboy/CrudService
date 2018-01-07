a823b513d1cab1e374a1c219c48a5656e4942f65
wlanboy <me@wlanboy.com>
2018-01-07 13:30:05 +0100

    Added Application, basic startup test and pom.xml

---
+++ src/main/java/com/wlanboy/demo/CrudServiceApplication.java
@@ -0,0 +1,25 @@
+package com.wlanboy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CrudServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudServiceApplication.class, args);
+	}
+	
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
+    }	
+}
