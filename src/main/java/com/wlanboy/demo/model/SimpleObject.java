2326e03fe7aa360ccb878f468c3a41ac76fde890
wlanboy <me@wlanboy.com>
2018-06-17 19:05:55 +0200

    Added SimpleObject Entity DTO and CrudRepository for REST endpoint.

---
+++ src/main/java/com/wlanboy/demo/model/SimpleObject.java
@@ -0,0 +1,26 @@
+package com.wlanboy.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "SimpleObject")
+public class SimpleObject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter public Long SIMPLE_ID;
+    
+    @Column(nullable = false, length = 60)
+    @Getter @Setter public String SIMPLE_NUMBER;
+    
+    @Column(nullable = false, length = 60)
+    @Getter @Setter public String SIMPLE_HASH;
+}
---
+++ src/main/java/com/wlanboy/demo/repository/SimpleObjectRepository.java
@@ -0,0 +1,16 @@
+package com.wlanboy.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import com.wlanboy.demo.model.SimpleObject;
+
+@CrossOrigin
+@Repository
+@RepositoryRestResource(path = "SimpleObject")
+public interface SimpleObjectRepository extends CrudRepository<SimpleObject, Long>{
+
+}
