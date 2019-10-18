922605808b221cd1554462d499c754e8d0b8d649
wlanboy <wlanboy@users.noreply.github.com>
2019-10-18 09:47:16 +0200

    Switched to slf4j and added lamdas

--- src/main/java/com/wlanboy/demo/clients/MirrorClientFallbackFactory.java
+++ src/main/java/com/wlanboy/demo/clients/MirrorClientFallbackFactory.java
@@ -1,8 +1,7 @@
 package com.wlanboy.demo.clients;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.wlanboy.demo.model.HelloParameters;
@@ -13,22 +12,20 @@ import feign.hystrix.FallbackFactory;
 @Component
 public class MirrorClientFallbackFactory implements FallbackFactory<MirrorClient> {
 
-	private static final Logger logger = Logger.getLogger(MirrorClientFallbackFactory.class.getCanonicalName());
+	private static final Logger logger = LoggerFactory.getLogger(MirrorClientFallbackFactory.class);
 	
 	@Override
 	public MirrorClient create(Throwable cause) {
 	     String httpStatus = cause instanceof FeignException ? Integer.toString(((FeignException) cause).status()) : "";
 	     String exception = cause instanceof FeignException ? ((FeignException) cause).getMessage() : "";
 
-	     return new MirrorClient() {
-			@Override
-			public HelloParameters postMirrorRequest(HelloParameters mirrorObject) {
-				logger.log(Level.SEVERE,String.format("Object: %s caused httpStatus %s.", mirrorObject,httpStatus));
+	     return ((HelloParameters mirrorObject) -> {
+				logger.error("Object: {} caused httpStatus {}", mirrorObject,httpStatus);
 				mirrorObject.setStatus(httpStatus);
 				mirrorObject.setTarget(exception);
 				return mirrorObject;
 			}
-	    };
+	    );
 	}
 
 }
